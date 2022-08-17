e5453920b83ef80215dd836fc46c0aef10829af2
aubbiali <aubbiali@sorint.it>
2022-08-17 11:43:12 +0200

    added comments

--- profExercises/Node/first-server/hello.js
+++ profExercises/Node/first-server/hello.js
@@ -4,16 +4,15 @@ const host = 'localhost';
 const port = 8000;
 const path = require('path');
 
-//const requestListener = function (req, res) {
-//    res.setHeader("Content-Type", "application/json");
-//    res.writeHead(200);
-//    res.end(`{"message": "This is a JSON response"}`);
-//};
+// ARRIVE 2 REQUESTS:
+// - "/" -> return the index.html file
+// - "/favicon.ico" -> directly requested by the browser, will be returned the ico but the second time it does the request return 500
+//                      because the code above add "index.html" to each requests.
 
 const requestListener = function (req, res) {
 	console.log("Richiesta ", req.url);
 	let filePath = path.join(__dirname, req.url);
-    fs.readFile(filePath)
+    fs.readFile(filePath + "index.html")
         .then(contents => {
             res.setHeader("Content-Type", "text/html");
             res.writeHead(200);
